6530a9e072fe933a306e0d2b0f9dfa79184d1ecd
PabloAzadian <pabloazadian09@gmail.com>
2024-06-12 16:19:26 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -7,17 +7,11 @@ import MilkyWay from './MilkyWay/MilkyWay';
 import PortalDelivery from './PortalDelivery/PortalDelivery';
 import Menu from './Menu/Menu';
 import Testimonials from "./Testimonials/Testimonials";
-<<<<<<< HEAD
 import Footer from "./Footer/Footer";
 import AboutUs from "./AboutUs/AboutUs";
-=======
-import Footer from "./Footer/Footer"
-import AboutUs from "./AboutUs/AboutsUs";
->>>>>>> 9aa6c990ebcc48314762507fe4e0aa78b6a0f122
 
 function App() {
   return (
-<<<<<<< HEAD
     <Router>
       <div className="App">
         <NavBar />
@@ -43,24 +37,6 @@ function Home() {
       <PortalDelivery />
       <Testimonials />
     </>
-=======
-    <div className="App">
-      <NavBar setScene={setScene}/>
-      <header>
-        {scene== "AboutUs" &&<AboutUs/>}
-        {scene== "Menu" &&<Menu/>}
-        {scene == "Home" && <>
-        <Hero setScene={setScene}/>
-        <IconSlider />
-        <MilkyWay setScene={setScene}/>
-        <PortalDelivery/>
-        <Testimonials/>
-        </>}
-        
-      </header>
-      <Footer />
-    </div>
->>>>>>> 9aa6c990ebcc48314762507fe4e0aa78b6a0f122
   );
 }
 
